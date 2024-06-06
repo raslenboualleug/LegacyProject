7ab69c27dd88b6434255074d18dfd294437ac95b
TahaBoughanmi <boughanmimohammedtaha@gmail.com>
2024-06-06 09:46:09 +0100

    T

--- legacy/app/page.tsx
+++ legacy/app/page.tsx
@@ -4,14 +4,13 @@ import Categories from "./home/Categories";
 import Thismonth from "./home/Thismonth";
 import Services from "./home/Services";
 import Featured from "./home/Featured";
-import Todays from "./home/Todays/page";
+import Todays from "./home/Todays";
 import Navbar from "./Navbar";
 import Navbanner from "./home/Navbanner";
 
 export default function Home() {
   return (
     <div>
-    
       <Navbar />
       <Navbanner />
       <Ourproducts />
